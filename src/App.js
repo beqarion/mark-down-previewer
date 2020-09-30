d57888fc701634b8bcccce31f1e4d7e2935bd8ec
Beka Shekiladze <beqarioni@yahoo.co.uk>
2020-09-30 15:07:05 +0400

    adding bootstrap and markdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = `
+  This is a paragraph
+   
+  **This is boloded text**
+  
+  # Heading
+  ## Heading 2
+  
+  > Block Quotes!  
+
+  - list item 1
+  - list item 2
+  - list item 3
+
+
+  [Visit my website](https://beqarion.github.io/my-resume.github.io/)
+
+  This is a inline \`<div></div>\`
+  
+  This is a block of code 
+  
+  \`\`\`
+    let x = 1;
+    let y = 2;
+    let z = x + y;
+  \`\`\`
+
+  ![React](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png)
+`;
+
+class App extends React.Component {
+   constructor(){
+     super();
+     this.state = {
+       text: initialState
+     }
+     this.handleChange = this.handleChange.bind(this);
+   }
+   handleChange(event){
+     this.setState({
+       text: event.target.value
+     })
+   }
+   render(){
+     const markdown = marked(this.state.text, {breaks: true});
+     return(
+      <div>
+        <h2 className="text-center m-4">Convert your Markdown</h2>
+        <div className="row">
+          <div className="col-6 par">
+            <h6 className="text-center">Enter your markdown here:</h6>
+            <textarea className="form-control p-2" id="editor" value={this.state.text} onChange={this.handleChange} />
+          </div>
+          <div className="col-6" >
+            <h6 className="text-center">Enter your markdown here:</h6>
+            <div className="preview rounded p-2" id="preview" dangerouslySetInnerHTML={{__html: markdown}} />
+          </div>
+        </div>
+      </div>
+      
+      
+     );
+   }
 }
 
 export default App;
